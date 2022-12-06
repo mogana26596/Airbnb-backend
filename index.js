d102d4c57c4fd4cdedfa763e2ae30f22677ad742
mogana26596 <108892214+mogana26596@users.noreply.github.com>
2022-12-06 14:37:36 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const mongo = require('./connect');
+const cors = require('cors');
+const dotenv = require('dotenv');
+const register = require('./router/registerRouter');
+const product = require('./router/productRouter');
+const auth = require("./modules/authModule");
+
+dotenv.config();
+mongo.connect();
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use('/register', register);
+app.use('/', auth.authenticateUser);
+app.use('/product', product);
+app.listen(process.env.PORT);
